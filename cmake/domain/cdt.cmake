685404f839104a83129cc171d1fb87895e969dde
HoonInPark <hooninpark@gmail.com>
2026-02-17 13:53:58 +0900

    domain dir added

--- cmake/domain/cdt.cmake
+++ cmake/domain/cdt.cmake
@@ -1,11 +1,11 @@
-get_filename_component(MODULE_NAME ${CMAKE_CURRENT_LIST_FILE} NAME_WE)
+get_filename_component(MODULE_NAME cdt.cmake NAME_WE)
 message(STATUS "Configuring for ${MODULE_NAME}")
 
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake/module")
 
+target_sources(${CMAKE_PROJECT_NAME} PRIVATE
+    src/CDT.cpp
+)
+
 ######## library ########
 include(cuda)
-
-target_sources(${CMAKE_PROJECT_NAME} PRIVATE
-        src/DT2D.cpp
-)
